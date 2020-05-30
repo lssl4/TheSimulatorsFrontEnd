64b130d843eb532b8dfcfc2aa57ebe20bbfd0eb2
Siong Leong <shaunleong@outlook.com>
2020-05-30 20:24:23 +0800

    Prelimenary layout

--- covid-impact-visualizer/src/App.js
+++ covid-impact-visualizer/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HorizontalTimeline from "react-horizontal-timeline";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const VALUES = ["2019-01-01", "2019-02-01", "2019-03-01"];
+
+class App extends React.Component {
+  state = { value: 0, previous: 0 };
+
+  render() {
+    return (
+      <div className="App">
+        <body>
+          <header className="App-header">World Map</header>
+          <div className="mapLayout">
+            <div className="mapElement">Map Element</div>
+            <div className="sidebarElement">Sidebar Element</div>
+          </div>
+
+          <div className="timeline">
+            <HorizontalTimeline
+              index={this.state.value}
+              indexClick={(index) => {
+                this.setState({ value: index, previous: this.state.value });
+              }}
+              values={VALUES}
+            />
+          </div>
+          <div>New one</div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
