ceda4a6ba6a5395b5ea665e453083edc2f88b774
mmazzaferro <38482559+mmazzaferro@users.noreply.github.com>
2020-05-30 20:10:39 +1000

    Added population density layer to map

--- covid-impact-visualizer/src/MapDisplay.js
+++ covid-impact-visualizer/src/MapDisplay.js
@@ -35,8 +35,8 @@ class MapDisplay extends Component {
                     source: new WMTSSource({
                         //TODO: first option gives real time map however data may not be available for current day yet
                         //      second option retrieves data at given date
-                        //url: 'https://gibs-{a-c}.earthdata.nasa.gov/wmts/epsg4326/best/wmts.cgi',
-                        url: 'https://gibs-{a-c}.earthdata.nasa.gov/wmts/epsg4326/best/wmts.cgi?TIME=2020-05-29',
+                        //url: 'https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/',
+                        url: 'https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/?TIME=2020-05-29',
                         layer: 'MODIS_Terra_CorrectedReflectance_TrueColor',
                         format: 'image/jpeg',
                         matrixSet: 'EPSG4326_250m',
@@ -56,7 +56,33 @@ class MapDisplay extends Component {
                           matrixIds: [0, 1, 2, 3, 4, 5, 6, 7, 8],
                           tileSize: 512
                         })
-                    })
+                    }),
+                    name: 'BaseLayer'
+                }),
+                new TileLayer({
+                    source: new WMTSSource({
+                        url: 'https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/',
+                        layer: 'GPW_Population_Density_2020',
+                        format: 'image/png',
+                        matrixSet: 'EPSG4326_1km',
+                        tileGrid: new WMTSTileGrid({
+                          origin: [-180, 90],
+                          resolutions: [
+                            0.5625,
+                            0.28125,
+                            0.140625,
+                            0.0703125,
+                            0.03515625,
+                            0.017578125,
+                            0.0087890625,
+                            0.00439453125,
+                            0.002197265625
+                          ],
+                          matrixIds: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+                          tileSize: 512
+                        })
+                    }),
+                    name: 'PopulationDensity'
                 })
             ],
             //Add in the following map controls
