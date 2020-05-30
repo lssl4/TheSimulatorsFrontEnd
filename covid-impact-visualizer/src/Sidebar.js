2ae3cce416fa177ea75b08e66b7f4d2e9ece7ad6
Ethan Pui <ethanpui@gmail.com>
2020-05-30 21:22:32 +0800

    Sidebar created
    Simple text and slider added for Population Density.

---
+++ covid-impact-visualizer/src/Sidebar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "./componentStyles.css"
+
+
+
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="sidebar">
+            <h2>Factors</h2>
+            
+        <label class="switch">
+        <input type="checkbox"></input>
+        <span class="slider round"></span>
+      </label>
+
+      <div className="population_text"> 
+            <b>Population Density</b> 
+          </div>
+
+      </div> 
+  );
+}
+export default App;
+
