e785d832d784ff73b8697ce5ca3ce7634da74bf0
Ethan Pui <ethanpui@gmail.com>
2020-05-30 22:21:51 +0800

    Made Sidebar a component

--- covid-impact-visualizer/src/Sidebar.js
+++ covid-impact-visualizer/src/Sidebar.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import './Sidebar.css';
+import "./components/Sidebar.js"
 import "./componentStyles.css"
 
 function App() {
   return (
-    <div className="sidebar">
-            <h2>Factors</h2>
-            
-        <label class="switch">
-        <input type="checkbox"></input>
-        <span class="slider round"></span>
-        </label>
-
-      <div className="population_text"> 
-            <b>Population Density</b> 
-        </div>
-
-    </div> 
+    <Sidebar/>
   );
 }
 export default App;
-
