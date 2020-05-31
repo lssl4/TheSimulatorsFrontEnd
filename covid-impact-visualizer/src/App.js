8be9207c8f7517dd34490fc65850717744a1ddb3
mmazzaferro <38482559+mmazzaferro@users.noreply.github.com>
2020-05-31 13:37:58 +1000

    Added line to export interface

--- covid-impact-visualizer/src/App.js
+++ covid-impact-visualizer/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import MapDisplay from "./MapDisplay";
-import "./Timeline";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
